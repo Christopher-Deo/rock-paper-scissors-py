b3c23340fff907a696d6e18f1cfba00480e97d30
Christopher-Deo <chris@christopherdeo.com>
2023-10-10 06:49:44 -0500

    completed hangman game exercise

--- main.py
+++ main.py
@@ -30,12 +30,12 @@ scissors = '''
 game_images = [rock, paper, scissors]
 
 user_choice = int(
-    input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print(game_images[user_choice])
+    eval(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")))
+print((game_images[user_choice]))
 
 computer_choice = random.randint(0, 2)
 print("Computer chose:")
-print(game_images[computer_choice])
+print((game_images[computer_choice]))
 
 if user_choice >= 3 or user_choice < 0:
   print("You typed an invalid number, you lose!")
